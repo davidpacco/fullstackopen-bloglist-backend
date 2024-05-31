c0a9cf852baf04344f0b6f5059cd16dec70f1094
davidpacco <david.j.pacco@gmail.com>
2024-05-31 18:38:21 -0500

    Add exercise 4.17 - blog list expansions step 5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const blogsRouter = express.Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
+  const { title, author, url } = req.body
+  const users = await User.find({})
+  const user = users[Math.floor(Math.random() * users.length)]
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    user: user._id
+  })
 
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   res.status(201).json(savedBlog)
 })
 
